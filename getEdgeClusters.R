110b334ceeffa7ee8373cf21ccd726a5be61b6e6
Nicholas D. Socci <soccin@mskcc.org>
2022-08-11 17:03:43 -0400

    test version 1 of edge script

--- getEdgeClusters.R
+++ getEdgeClusters.R
@@ -6,6 +6,7 @@ Usage:
 Options:
   -v --verbose   Printing debugging/logging info
   -i=<ins> --ins=<ins> Insert Size [default: 250]
+  --rc=<rc>      Set readCounts/size filter [default: 0]
   --rv=<rv>      Set RV filter [default: 0]
   --vaf=<vaf>    Set VAF {RV/(RR+RV)} filter [default: 0]
 
@@ -69,12 +70,31 @@ get_event_clusters<-function(sampleManifest,vcfFolder) {
         return(NULL)
     }
 
-    events_intersect=genome_intersect(events_filtered,events_filtered,by=c("CHROM","POS","END")) %>% filter(POS!=END)
+
+    evts_5p=events_filtered %>%
+        mutate(
+            SIDE='5p',
+            POS=POS-floor(insertSize/2),
+            END=POS+floor(insertSize/2),
+            UUID=cc(UUID,SIDE)
+        )
+
+    evts_3p=events_filtered %>%
+        mutate(
+            SIDE='3p',
+            POS=END-floor(insertSize/2),
+            END=END+floor(insertSize/2),
+            UUID=cc(UUID,SIDE)
+        )
+
+    events_edges=bind_rows(evts_5p,evts_3p) %>% select(-SIZE)
+
+    events_intersect=genome_intersect(events_edges,events_edges,by=c("CHROM","POS","END")) %>% filter(POS!=END)
 
     edges=events_intersect %>%
         filter(UUID.x<UUID.y) %>%
-        mutate(SIZE_AVG=(SIZE.x+SIZE.y)/2,PCT_OVER=(END-POS)/SIZE_AVG) %>%
-        filter((SIZE.x<1e5 & SIZE.y<1e5) | PCT_OVER>0.001) %>%
+        mutate(PCT_OVER=(END-POS)/insertSize) %>%
+        filter(PCT_OVER>0.1) %>%
         select(matches("UUID"))
 
     if(nrow(edges)==0) {
@@ -85,7 +105,7 @@ get_event_clusters<-function(sampleManifest,vcfFolder) {
 
     event_cliques=max_cliques(eventGraph)
 
-    clusters=clique_to_event_table(event_cliques,events_filtered)
+    clusters=clique_to_event_table(event_cliques,events_edges)
 
     clusters
 
@@ -95,7 +115,9 @@ clusters=get_event_clusters(argv$sampleManifest,argv$vcfFolder)
 
 if(!is.null(clusters) && nrow(clusters)>0) {
 
-    outFile=cc("clusters","EDGE","Small,Large_0.001_Overlap","",
+    outFile=cc("clusters","EDGE",
+                    "INSSize",insertSize,
+                    "Small,Large_0.001_Overlap","",
                     "RCFilter",sprintf("%05d",rcFilter),
                     "RVFilter",sprintf("%05d",rvFilter),
                     "VAFFilter",sprintf("%07.03f",round(100*vafFilter,3)),
