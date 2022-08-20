18efd94c8e22a48e7480285c8bb4c5111ca97fe6
Nicholas D. Socci <soccin@mskcc.org>
2022-08-20 00:08:20 -0400

    bugfix

--- getMutectEvents.R
+++ getMutectEvents.R
@@ -7,8 +7,8 @@ suppressPackageStartupMessages({
 source("P5BrainReorg/tools.R")
 
 verbose=T
-vcfFiles=scan("mutect2VCFs","")
+vcfFiles=scan("P5BrainReorg/mutect2VCFs","")
 
-vv=read_mutect2_events("raw/manifest_B-101-295__DellyVCFs_V2.xlsx")
+vv=read_mutect2_events("raw/manifest_B-101-295__DellyVCFs_V2.xlsx",vcfFiles)
 
 vv %>% select(SID,ETAG) %>% group_by(ETAG) %>% summarize(SSID=paste0(SID,collapse=";"))
